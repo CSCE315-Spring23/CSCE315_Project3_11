f4135174bc379ed65e9dd5ef90b10c9f49925f23
drew654 <drewpusey654@gmail.com>
2023-04-04 18:19:40 -0500

    Merge conflict editor being annoying

--- Project3_11/pos/views.py
+++ Project3_11/pos/views.py
@@ -28,3 +28,24 @@ def employee_page(request):
     else:
         content = {'data': 'test hi'}
         return render(request, 'employee.html', content)
+
+
+def menuItems(request):
+    fullMenu = MenuItem.objects.all().values()
+    print(fullMenu)
+    content = {'menuTest': fullMenu}
+    return HttpResponse(render(request, 'employee.html', content))
+
+
+def database_info(request):
+    if request.method == 'GET':
+        employees = Employee.objects.all()
+        expiration_dates = ExpirationDate.objects.all()
+        inventory_items = InventoryItem.objects.all()
+        menu_items = MenuItem.objects.all()
+        orders = Order.objects.all()
+        restock_orders = RestockOrder.objects.all()
+        z_reports = ZReport.objects.all()
+        context = {'employees': employees, 'expiration_dates': expiration_dates, 'inventory_items': inventory_items,
+                   'menu_items': menu_items, 'orders': orders, 'restock_orders': restock_orders, 'z_reports': z_reports}
+        return render(request, 'database_info.html', context)
