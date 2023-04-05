9f9b04a607e6769863851f201f0fa037af2adb43
Anthony Noyes <anthonynoyes16@tamu.edu>
2023-04-05 12:13:31 -0500

    created inventory item functions and test cases

---
+++ Project3_11/pos/inventory_functions.py
@@ -0,0 +1,40 @@
+from pos.models import InventoryItem
+
+
+def findInventoryItem(name):
+    try:
+        item = InventoryItem.objects.get(Name=name)
+        return {
+            "Name": item.Name,
+            "Stock": item.Stock,
+            "NumberNeeded": item.NumberNeeded,
+            "OrderChance": item.OrderChance,
+            "Units": item.Units,
+            "Category": item.Category,
+            "Servings": item.Servings,
+            "RestockCost": item.RestockCost,
+        }
+    except InventoryItem.DoesNotExist:
+        return None
+
+def addInventoryItem(name="null", stock=-1, number_needed=-1, order_chance=-1.0, units="null", category="null", servings=-1, restock_cost=-1):
+    # create a new inventory item object with the given attributes
+    new_item = InventoryItem(
+        Name=name,
+        Stock=stock,
+        NumberNeeded=number_needed,
+        OrderChance=order_chance,
+        Units=units,
+        Category=category,
+        Servings=servings,
+        RestockCost=restock_cost
+    )
+    # save the new item to the database
+    new_item.save()
+
+def removeInventoryItem(name):
+    try:
+        item = InventoryItem.objects.get(Name=name)
+        item.delete()
+    except InventoryItem.DoesNotExist:
+        print(f"Inventory item '{name}' not found in database.")
--- Project3_11/pos/tests.py
+++ Project3_11/pos/tests.py
@@ -1,7 +1,11 @@
 from django.test import TestCase
 import json
+
+import pos.inventory_functions
 from pos.models import Employee
 from pos.models import MenuItem
+from pos.inventory_functions import *
+
 
 # Print the name of an Employee given their ID
 employee1 = Employee.objects.get(EmployeeID=1)
@@ -16,3 +20,13 @@ bowl_item.Price = 9.99
 full_menu = MenuItem.objects.all()
 for item in full_menu:
     print(item.ItemName + " costs " + str(item.Price))
+
+# find an inventory item info by name
+item_dict = findInventoryItem("Spinach")
+print(item_dict["Stock"])
+
+# add an inventory item, specify whichever fields you want - the others will be put to null values
+addInventoryItem(name="Test", order_chance=0.5)
+
+# remove an inventory item by name
+removeInventoryItem("Test")
