3bd70fe999d11cf33db52c033d11642868182b53
alex-gonce <alex-gonce@tamu.edu>
2023-04-10 11:58:22 -0500

    added button to go to menuItems page

--- Project3_11/pos/urls.py
+++ Project3_11/pos/urls.py
@@ -6,6 +6,6 @@ urlpatterns = [
     path('', views.login, name='login'),
     path('employee/', views.employee_page, name='employee'),
     path('order/', views.order_page, name='order'),
-    path('menuItems/', views.menuItems, name='employee'),
+    path('menuItems/', views.menuItems, name='menuItems'),
     path('database_info/', views.database_info, name='database_info'),
 ]
