8234e0e3d32f395fd260238290d7541e334908c6
alex-gonce <alex-gonce@tamu.edu>
2023-05-02 12:31:03 -0500

    cleaned up edit inventory item

--- Project3_11/pos/urls.py
+++ Project3_11/pos/urls.py
@@ -12,7 +12,6 @@ urlpatterns = [
     path('', views.login, name='login'),
     path('employee/', views.employee_page, name='employee'),
     path('order/', views.order_page, name='order'),
-    path('inventory/', views.inventory_page, name='inventory'),
     path('reports/', views.reports_page, name='reports'),
     path('menuItems/', views.menuItems, name='menuItems'),
     path('database_info/', views.database_info, name='database_info'),
